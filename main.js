9000f0f0ecd775d5b7e7e6fa41537a2f7f243c1c
MatiRdez <matiasnrz5@gmail.com>
2023-05-22 16:29:47 -0300

    Cálculos de encriptación

--- main.js
+++ main.js
@@ -17,5 +17,10 @@ const encriptar = () => {
     for (let i = 0; i < cadenaA.length; i++){
         digitosA.push(parseInt(cadenaA[i], 10));
     }
+
+    let digitoFinalA1 = ((digitosA[0] + 7) % 10);
+    let digitoFinalA2 = ((digitosA[1] + 7) % 10);
+    let digitoFinalA3 = ((digitosA[2] + 7) % 10);
+    let digitoFinalA4 = ((digitosA[3] + 7) % 10);
     
 }
