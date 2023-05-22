85a33b1e2bdcfbeb3af1c0113b7e20e6e2414164
MatiRdez <matiasnrz5@gmail.com>
2023-05-22 15:53:11 -0300

    Desactivando botón encriptar según condición

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
 let inputA = document.getElementById("inputA");
+
+inputA.addEventListener("input", () => {
+    if(inputA.value.length === 4){
+        document.getElementById("enviarA").disabled = false;
+    }
+    else{
+        document.getElementById("enviarA").disabled = true;
+    }
+});
